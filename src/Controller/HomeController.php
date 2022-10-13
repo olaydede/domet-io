5f63e69b2af097b2669b465675a694c3b99b3c0b
Olay Dede <olay.dede@collective-it.be>
2022-10-13 09:00:48 +0200

    Added User Entity and simple authentication

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,13 +1,17 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name:'home')]
-    public function index()
+    public function index(UserRepository $repository, UserPasswordHasherInterface $hasher)
     {
         return $this->render('home.html.twig', []);
     }
